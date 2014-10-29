47d43536a0edeca53664881e46489f16b25582da
Jorge Arévalo <jorge@cartodb.com>
2014-10-29 21:42:39 +0100

    Bugs fixed to make the script work with a couple of BBVA API calls and one CartoDB API call

--- bbva2cartodb.py
+++ bbva2cartodb.py
@@ -10,6 +10,7 @@ from os import environ
    Make an API call to BBVA API and put the result into a CartoDB table.
 """
 
+bbva_url_most_popular_tiles = "https://apis.bbvabancomer.com/datathon/info/tiles"
 bbva_url_stats = "https://apis.bbvabancomer.com/datathon/tiles/%s/%s/cards_cube"
 cartodb_url_sql = "http://%s.cartodb.com/api/v2/sql"
 
@@ -57,91 +58,94 @@ def main(argv):
     #We have all the arguments for the query stored in a text file
     bbva_payload = read_payload_from_file(argv[1])
 
-    sql = "SELECT longitude, latitude from demoscartodb.bytile order by total desc offset 135"
 
-    payload = {'q': sql, 'api_key': cdb_api_key}
-    r = requests.get(cartodb_url_sql % cdb_user, params=payload)
+    #Now, get the ranking of the most active zip codes (with the greatest number of payments) in descending order
+    r = requests.get(bbva_url_most_popular_tiles, headers=headers)
 
     data = r.json()
 
-    if 'rows' in data:
-        for point in data['rows']:
 
-            #Raise the BBVA API query
-            if 'latitude' in point and 'longitude' in point:
-                r2 = requests.get(bbva_url_stats % (point['latitude'], point['longitude']), params=bbva_payload, headers=headers)
-
-                #print r2.url
-
-                data2 = r2.json()
-
-                if 'metadata' in data2 and 'hash_description' in data2['metadata'] and 'ranges' in data2['metadata']['hash_description'] and 'data' in data2 and 'stats' in data2['data']:
+    if 'data' in data and 'coordinates' in data['data']:
 
 
-                    # First, parse metadata
-                    genders = {}
-                    ages = {}
+        for point in data['data']['coordinates']:
 
-                    for range in data2['metadata']['hash_description']['ranges']:
+            longitude = point[0]
+            latitude = point[1]
 
-                        if 'name' in range and 'values' in range:
+            r2 = requests.get(bbva_url_stats % (latitude, longitude), params=bbva_payload, headers=headers)
 
-                            if range['name'] == 'gender':
-                                for gender in range['values']:
-                                    if 'label' in gender and 'description' in gender:
-                                        genders[str(gender['label'])] = str(gender['description'])
-
-                            elif range['name'] == 'age':
-                                for age in range['values']:
-                                    if 'label' in age and 'description' in age:
-                                        ages[str(age['label'])] = str(age['description'])
+            data2 = r2.json()
 
 
-                    #Now, parses data
-                    for stat in data2['data']['stats']:
+            if 'metadata' in data2 and 'hash_description' in data2['metadata'] and 'ranges' in data2['metadata']['hash_description'] and 'data' in data2 and 'stats' in data2['data']:
 
 
-                        insert_dict = {}
+                # First, parse metadata
+                genders = {}
+                ages = {}
+
+                for range in data2['metadata']['hash_description']['ranges']:
+
+                    if 'name' in range and 'values' in range:
+
+                        if range['name'] == 'gender':
+                            for gender in range['values']:
+                                if 'label' in gender and 'description' in gender:
+                                    genders[str(gender['label'])] = str(gender['description'])
+
+                        elif range['name'] == 'age':
+                            for age in range['values']:
+                                if 'label' in age and 'description' in age:
+                                    ages[str(age['label'])] = str(age['description'])
 
 
-                        if 'date' in stat:
-                            insert_dict['the_date'] = "'" + str(stat['date']) + "'"
-
-                        if 'cube' in stat:
-                            cubes = stat['cube']
-
-                            # Here, we've flatenning the structure, to fit into our data model
-                            for cube in cubes:
-
-                                if 'hash' in cube:
-                                    l = str(cube['hash']).split('#')
-
-                                    insert_dict['age'] = "'" + ages[l[1]] + "'"
-                                    insert_dict['gender'] = "'" + genders[l[0]] + "'"
-
-                                if 'num_cards' in cube:
-                                    insert_dict['num_cards'] = str(cube['num_cards'])
+                #Now, parses data
+                for stat in data2['data']['stats']:
 
 
-                                if 'num_payments' in cube:
-                                    insert_dict['num_payments'] = str(cube['num_payments'])
+                    insert_dict = {}
 
 
-                                if 'avg' in cube:
-                                    insert_dict['avg'] = str(cube['avg'])
+                    if 'date' in stat:
+                        insert_dict['the_date'] = "'" + str(stat['date']) + "'"
 
 
+                    if 'cube' in stat:
+                        cubes = stat['cube']
 
-                                insert_dict['the_geom'] = "ST_SetSRID(ST_MakePoint(%s, %s), 4326)" % (str(point['longitude']),str(point['latitude']))
+                        # Here, we've flatenning the structure, to fit into our data model
+                        for cube in cubes:
 
 
-                                sql2 = "insert into stats_by_gender_and_age(%s) values(%s)" % (','.join(insert_dict.keys()),','.join(insert_dict.values()))
+                            if 'hash' in cube:
+                                l = str(cube['hash']).split('#')
 
-                                print sql2
+                                insert_dict['age'] = "'" + ages[l[1]] + "'"
+                                insert_dict['gender'] = "'" + genders[l[0]] + "'"
 
-                                #Insert entry in CartoDB
-                                payload2 = {'q': sql2, 'api_key': cdb_api_key}
-                                r3 = requests.get(cartodb_url_sql % cdb_user, params=payload2)
+                            if 'num_cards' in cube:
+                                insert_dict['num_cards'] = str(cube['num_cards'])
+
+
+                            if 'num_payments' in cube:
+                                insert_dict['num_payments'] = str(cube['num_payments'])
+
+
+                            if 'avg' in cube:
+                                insert_dict['avg'] = str(cube['avg'])
+
+
+                            insert_dict['the_geom'] = "ST_SetSRID(ST_MakePoint(%d, %d), 4326)" % (longitude, latitude)
+
+
+                            sql2 = "insert into stats_by_gender_and_age(%s) values(%s)" % (','.join(insert_dict.keys()),','.join(insert_dict.values()))
+
+                            print sql2
+
+                            #Insert entry in CartoDB
+                            #payload2 = {'q': sql2, 'api_key': cdb_api_key}
+                            #r3 = requests.get(cartodb_url_sql % cdb_user, params=payload2)
 
 
 
